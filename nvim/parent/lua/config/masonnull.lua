032a5192c6683c37d17e3f15ece9c705161ef121
ModalDiagram <stodgy_goilg@aleeas.com>
2023-04-23 14:33:22 +0200

    updated deprecated config

--- nvim/parent/lua/config/masonnull.lua
+++ nvim/parent/lua/config/masonnull.lua
@@ -1,6 +1,15 @@
 local null_ls = require 'null-ls'
 
 require ('mason-null-ls').setup({
+  handlers = {
+    function(source_name, methods)
+      -- all sources with no handler get passed here
+
+      -- To keep the original functionality of `automatic_setup = true`,
+      -- please add the below.
+      require("mason-null-ls.automatic_setup")(source_name, methods)
+    end,
+  },
 })
 
 -- require ('mason-null-ls').setup_handlers {
