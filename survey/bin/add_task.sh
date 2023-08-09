cd265d0b7405c96bcece8d1f8f71eb97b722d62d
ModalDiagram <stodgy_goilg@aleeas.com>
2023-08-09 09:39:07 +0200

    Fixed updated ydotool2

--- survey/bin/add_task.sh
+++ survey/bin/add_task.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-ydotool mousemove -a 600 370
+YDOTOOL_SOCKET="$HOME/.ydotool_socket" ydotool mousemove -a 600 370
 sleep 0.2
-ydotool click 0xC0
+YDOTOOL_SOCKET="$HOME/.ydotool_socket" ydotool click 0xC0
 $DOTFILES/survey/bin/add
 notify-send -t 5000 "inviata"
 pkill -RTMIN+13 waybar
