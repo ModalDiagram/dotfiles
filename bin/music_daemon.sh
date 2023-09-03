335bdf10b63b4d986daaa09fbd53958207db3c73
ModalDiagram <stodgy_goilg@aleeas.com>
2023-09-03 12:19:24 +0200

    daemon now replaces the same notification while it is running

--- bin/music_daemon.sh
+++ bin/music_daemon.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+# ./music_daemon.sh launches the daemon
+# While it is running, it sends notification everytime the playing song changes
+# (actually it saves the id of the notification so that it can replace the
+# same one notification).
+#
+# At the start, it writes its pid in $file, so that
+# ./music_daemon.sh kill
+# kills the daemon.
+#
+# This is useful when you change song from outside Spotify but don't always
+# want notifications
 
 file=/tmp/music_daemon.lock
 
@@ -16,6 +27,7 @@ fi
 echo $$ > "$file"
 
 IFS=$'\t'
+current_id=-1
 # if [[ "$status" == "Stopped" ]]; then printf ""; exit; fi
 while read -r playing artist title; do
   if [[ "$playing" == ⏹️ ]]; then printf '{"text":}\n';
@@ -26,10 +38,10 @@ while read -r playing artist title; do
     # echo "$playing";
     # echo "$artist";
     # echo "$title";
-    notify-send -t 10000 "Playing $title by $artist"
-    # text="$playing $title"
-    # tooltip="$text $artist"
-    #   printf '{"text":"%s","tooltip":"%s"}\n' \
-    #     "$text" "$tooltip"
+    if [[ current_id -eq "-1" ]]; then
+      current_id=$(notify-send -p -a spotify -t 10000 "Playing $title by $artist")
+    else
+      notify-send -r "$current_id" -a spotify -t 10000 "Playing $title by $artist"
+    fi
   fi
 done < <(playerctl --follow metadata --player playerctld --format                        $':{{emoji(status)}}\t:{{markup_escape(artist)}}\t:{{markup_escape(title)}}')
