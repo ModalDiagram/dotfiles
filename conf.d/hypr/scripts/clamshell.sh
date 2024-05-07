503ebe822047ec9e0451177a2b099041c1a27cdb
ModalDiagram <git@sanfio.eu>
2024-05-07 22:46:21 +0200

    clamshell

--- conf.d/hypr/scripts/clamshell.sh
+++ conf.d/hypr/scripts/clamshell.sh
@@ -1,21 +1,15 @@
 #!/bin/sh
 
-if [[ "$1" == "open" ]]; then
+# if [[ "$1" == "open" ]]; then
+#   hyprctl keyword monitor "eDP-1,2880x1800@60,auto,2"
+# else
+#   hyprctl keyword monitor "eDP-1,disable"
+# fi
+
+if [[ "$1" == "open" && ! "$(hyprctl monitors)" =~ "eDP-1" ]]; then
   hyprctl keyword monitor "eDP-1,2880x1800@60,auto,2"
-else
+  return
+fi
+if [[ "$(hyprctl monitors)" =~ "HDMI-A-1" ]]; then
   hyprctl keyword monitor "eDP-1,disable"
 fi
-
-# if [[ "$(hyprctl monitors)" =~ "HDMI-A-1" ]]; then 
-#   if [[ "$1" == "open" ]]; then
-#     hyprctl keyword monitor "eDP-1,2880x1800@60,auto,2"
-#   else
-#     hyprctl keyword monitor "eDP-1,disable"
-#   fi
-# else
-#   if [[ "$1" == "open" && "$(hyprctl monitors)" =~ "eDP-1" ]]; then
-#     ~/.local/share/my_lock/my_lock.sh
-#   else
-#     hyprctl keyword monitor "eDP-1,2880x1800@60,auto,2"
-#   fi
-# fi
