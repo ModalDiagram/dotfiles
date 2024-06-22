af2f3bfa250b1602b1681c68f56643edeb2a7996
ModalDiagram <git@sanfio.eu>
2024-06-22 20:49:17 +0200

    fixed when no option is selected

--- conf.d/hypr/scripts/monitor_disabler.sh
+++ conf.d/hypr/scripts/monitor_disabler.sh
@@ -2,7 +2,9 @@
 #
 #
 monitor=$(printf "eDP-1\nHDMI-A-1" | wofi -i --allow-images --height 210 --dmenu --cache-file /dev/null)
+if [[ -z $monitor ]]; then exit; fi
 mode=$(printf "disable\ndpms off" | wofi -i --allow-images --height 210 --dmenu --cache-file /dev/null)
+if [[ -z $mode ]]; then exit; fi
 
 if [[ $mode == "disable" ]]; then
   hyprctl keyword monitor $monitor,disable
