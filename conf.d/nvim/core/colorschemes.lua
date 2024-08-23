2e7caa8116d602c221a295d674e0b422581f668f
ModalDiagram <git@sanfio.eu>
2024-08-23 22:16:50 +0200

    .

--- conf.d/nvim/core/colorschemes.lua
+++ conf.d/nvim/core/colorschemes.lua
@@ -2,3 +2,4 @@ require("NeoSolarized").setup {
   transparent = false,
 }
 vim.cmd[[colorscheme NeoSolarized]]
+vim.api.nvim_set_hl(0, 'LeapLabelPrimary', { fg = 'white', bg = "black", bold = true })
